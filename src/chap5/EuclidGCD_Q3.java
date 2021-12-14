adf368bba84235e714ee363398a2278b05a6179e
fivingo <fiving@naver.com>
2021-12-15 01:26:32 +0900

    21.12.15

---
+++ src/chap5/EuclidGCD_Q3.java
@@ -0,0 +1,42 @@
+package chap5;
+
+import java.util.Scanner;
+
+// 유클리드 호제법으로 최대공약수 구하기
+public class EuclidGCD_Q3 {
+
+    // 정수 x, y의 최대공약수를 구하여 반환합니다.
+    static int gcd(int x, int y) {
+        if (y == 0) {
+            return x;
+        } else {
+            return gcd(y, x % y);
+        }
+    }
+
+    static int gcdArray(int[] a, int start, int size) {
+        if (size == 1) {
+            return a[start];
+        } else if (size == 2) {
+            return gcd(a[start], a[start + 1]);
+        } else {
+            return gcd(a[start], gcdArray(a, start + 1, size - 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("몇 개의 정수의 최대 공약수?: ");
+        int num = stdIn.nextInt();
+
+        int[] a = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            System.out.print("정수 입력: ");
+            a[i] = stdIn.nextInt();
+        }
+
+        System.out.println("최대공약수는 " + gcdArray(a, 0, num) + "입니다.");
+    }
+}
---
+++ src/chap5/Factorial_Q1.java
@@ -0,0 +1,27 @@
+package chap5;
+
+import java.util.Scanner;
+
+// 팩토리얼을 비재귀적으로 구현
+public class Factorial_Q1 {
+
+    // 양의 정수 n의 팩토리얼을 반환합니다.
+    static int factorial(int n) {
+        int fact = n;
+
+        while (n > 1) {
+            fact *= n--;
+        }
+
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("정수를 입력하세요: ");
+        int x = stdIn.nextInt();
+
+        System.out.println(x + "의 팩토리얼은 " + factorial(x) + "입니다.");
+    }
+}

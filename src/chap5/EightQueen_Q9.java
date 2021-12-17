207ad2341c98c8ac23dac7aee8ad8dbf84ce4b81
fivingo <fiving@naver.com>
2021-12-17 19:16:32 +0900

    21.12.17

---
+++ src/chap5/EightQueen_Q9.java
@@ -0,0 +1,61 @@
+package chap5;
+
+// 8 퀸 문제 풀이
+public class EightQueen_Q9 {
+    static boolean[] flag_a = new boolean[8];   // 각 행에 퀸을 배치했는지 체크
+    static boolean[] flag_b = new boolean[15];  // / 대각선 방향으로 퀸을 배치했는지 체크
+    static boolean[] flag_c = new boolean[15];  // \ 대각선 방향으로 퀸을 배치했는지 체크
+    static int[] pos = new int[8];              // 각 열의 퀸의 위치
+
+    // 각 열의 퀸의 위치를 출력
+    static void print()  {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.printf("%2s", j == pos[i] ? "■" : "□");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    // i열의 알맞은 위치에 퀸을 배치
+    static void set(int i) {
+        int j;
+        int[] jStack = new int[8];
+
+        Start: while (true) {
+            j = 0;
+
+            while (true) {
+                while (j < 8) {
+                    if (!flag_a[j] && !flag_b[i + j] && !flag_c[i - j + 7]) {
+                        pos[i] = j;
+
+                        if (i == 7) {           // 모든 열에 배치 마침
+                            print();
+                        } else {
+                            flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = true;
+                            jStack[i++] = j;    // i열의 행을 push
+
+                            continue Start;
+                        }
+                    }
+
+                    j++;
+                }
+
+                if (--i == -1) {
+                    return;
+                }
+
+                j = jStack[i];                  // i열의 행을 pop
+                flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = false;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        set(0);
+    }
+}
